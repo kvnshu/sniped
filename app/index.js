4e4d0d5bf0c1622c96fe9c7d2fb7a38a52afbc84
williamkuc <64158177+williamkuc@users.noreply.github.com>
2023-11-11 17:07:33 -0500

    combine home view to integrate auth

--- app/index.js
+++ app/index.js
@@ -1,29 +1,13 @@
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-// import Account from './(tabs)/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-import { Redirect } from 'expo-router'
-import Login from './(auth)/login'
+
+
 
 
 export default function App() {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+  
 
   return (
     <View>
-      {session && session.user ? <Redirect href="/FeedView" /> : <Login />}
+      { ? <Redirect href="/FeedView" /> : }
     </View>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,49 +1,71 @@
-import React from 'react';
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
-import { useFonts, Inter_900Black, Inter_200ExtraLight, Inter_600SemiBold, Inter_700Bold, Inter_400Regular } from '@expo-google-fonts/inter';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import FirstNameInputView from './(auth)/FirstNameInputView';
-import { Redirect } from 'expo-router';
+import 'react-native-url-polyfill/auto'
+import React, { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_200ExtraLight,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
+
+import Login from "./(auth)/login";
 
 const StartPage = () => {
-    const loggedIn = true;
+  const [session, setSession] = useState(null);
 
-    let [fontsLoaded] = useFonts({
-        Inter_900Black,
-        Inter_200ExtraLight,
-        Inter_400Regular,
-        Inter_600SemiBold,
-        Inter_700Bold,
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
     });
 
-    if (!fontsLoaded) {
-        return (
-            <View style={styles.loadingContainer}>
-                <ActivityIndicator size="large" color="#fff" />
-            </View>
-        );
-    }
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_200ExtraLight,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
     return (
-        <View style={styles.fullScreen}>
-            <SafeAreaProvider>
-                {loggedIn ? <Redirect href="/FeedView" /> : <FirstNameInputView />}
-            </SafeAreaProvider>
-        </View>
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
     );
+  }
+
+  return (
+    <View style={styles.fullScreen}>
+      <SafeAreaProvider>
+        {/*TODO fix logic to allow new users to input name*/} 
+        {session && session.user ? <Redirect href="/FeedView" /> : <Login />}
+      </SafeAreaProvider>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    fullScreen: {
-        flex: 1,
-        backgroundColor: '#000',
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#000',
-    },
+  fullScreen: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
 });
 
 export default StartPage;
