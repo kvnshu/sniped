ea5aca0c1d9f890d6d84159c9d189f1c950157ad
K3vX21 <54183095+kvnshu@users.noreply.github.com>
2023-11-12 05:02:37 -0500

    humanize post timestamps

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { View, Image, StyleSheet, Dimensions, Text } from "react-native";
 import PostDetails from "./PostDetails";
-import { formatDistanceToNow, parse } from "date-fns";
+import { formatDistanceToNow, parseISO } from "date-fns";
 
 // Get screen width
 const screenWidth = (Dimensions.get("window").width * 9) / 10;
 // Calculate height for 3:4 aspect ratio
 const imageHeight = (screenWidth * 6) / 5;
 
-const Post = ({ post }) => (
-  <View style={styles.postContainer}>
-    <PostDetails
-      creatorUsername={post.creatorUsername}
-      taggedUsername={post.taggedUsername}
-      creatorProfilePic={post.creatorProfilePic}
-      taggedProfilePic={post.taggedProfilePic}
-      timestamp={post.timestamp}
-    />
-    <Image
-      source={{ uri: post.postPicture }}
-      style={[styles.postImage, { width: screenWidth, height: imageHeight }]}
-    />
-    <Text style={styles.timestamp}>Sniped {post.timestamp}</Text>
-  </View>
-);
+const Post = ({ post }) => {
 
+  function parseAndHumanizeDate(dateString) {
+    console.log(dateString)
+    const parsedDate = parseISO(dateString);
+    return formatDistanceToNow(parsedDate, { addSuffix: true });
+  }
+
+  return (
+    <View style={styles.postContainer}>
+      <PostDetails
+        creatorUsername={post.creatorUsername}
+        taggedUsername={post.taggedUsername}
+        creatorProfilePic={post.creatorProfilePic}
+        taggedProfilePic={post.taggedProfilePic}
+        timestamp={post.timestamp}
+      />
+      <Image
+        source={{ uri: post.postPicture }}
+        style={[styles.postImage, { width: screenWidth, height: imageHeight }]}
+      />
+      <Text style={styles.timestamp}>Sniped {parseAndHumanizeDate(post.timestamp)}</Text>
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   timestamp: {
     paddingTop: 5,
